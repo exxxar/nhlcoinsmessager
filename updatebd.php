08956acfb52aeb6921a6fb7d5f20e8eb8960fcc0
Алексей Егеньевич <exxxar@users.noreply.github.com>
2018-10-04 22:01:26 +0300

    фикс работы с бд

--- updatebd.php
+++ updatebd.php
@@ -1,29 +1,14 @@
 <?php
 
+require_once("../../../wp-load.php");
 
 $mail = $_POST['mail'];
 $name = $_POST['name'];
 
-global $wpdb;
+ global $wpdb;
 
-$wpdb->show_errors();
+$wpdb->get_results( "INSERT INTO `wp_limi_forwards`( `player_name`, `pay_mail`, `pay_skype`, `final_koplate`, `final_coins`, `final_rekvisitu`, `final_msg`) VALUES ('$name','$mail','','','','','')" );
 
-
-echo var_dump($wpdb->get_row( "SELECT * FROM `wp_limi_forwards`"));
-
-$wpdb->print_error();
-//SELECT * FROM `wp_limi_forwards`
-/*
-try {
-    global $wpdb;
-
-    $wpdb->get_results( "INSERT INTO `wp_limi_forwards`( `player_name`, `pay_mail`, `pay_skype`, `final_koplate`, `final_coins`, `final_rekvisitu`, `final_msg`) VALUES ('$name','$mail','','',','','')" );
-} catch (Exception $e) {
-    echo 'Выброшено исключение: ',  $e->getMessage(), "\n";
-}
-
-
-*/
 
 
 echo $mail.' '.$name;
